dd88d2cdb29e93321b46da91294c1d3e78eeeb6c
Fauzan Taqiyuddin <19102103@ittelkom-pwt.ac.id>
2022-09-03 03:16:29 +0700

    first commit 2

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import cors from 'cors'
 
 const app = express()
 
-const host = process.env.HOST || undefined
-const port = parseInt(process.env.PORT ?? 8000)
+// const host = process.env.HOST || undefined
+const port = 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -20,11 +20,9 @@ const listenerCallback = () => {
     console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
 }
 
-if (host) {
-    app.listen(port, host, listenerCallback)
-} else {
-    app.listen(port, listenerCallback)
-}
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 nodeCleanup(cleanup)
 
